573146ea7ea9caee473b061767d75b53a22e5f46
Eliot S <eliot.solbrig@gmail.com>
2023-07-16 11:57:11 -0400

    Added components folder

---
+++ lib/components/DrugLogPreview.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:druglog2/models/DrugLog.dart';
+
+class DrugLogPreview extends StatefulWidget {
+  // const DrugLogPreview({super.key}, required this.drugLog);
+  final DrugLog drugLog;
+  const DrugLogPreview(this.drugLog);
+
+  @override
+  State<DrugLogPreview> createState() => _DrugLogPreviewState();
+}
+
+class _DrugLogPreviewState extends State<DrugLogPreview> {
+  // static DrugLog testDrugLog = DrugLog(title: 'test');
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            width: 200,
+            height: 20,
+            child: Text('DrugLog name:  ${widget.drugLog.title}'),
+          ),
+        ]);
+  }
+}
