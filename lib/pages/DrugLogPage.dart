811119f688233cf44e8acf985d3debba51c77b22
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2023-07-16 13:02:54 -0400

    WIP can't get selected drug to update

--- lib/pages/DrugLogPage.dart
+++ lib/pages/DrugLogPage.dart
@@ -13,7 +13,6 @@ class DrugLogPage extends StatefulWidget {
 class _DrugLogPageState extends State<DrugLogPage> {
   List<Entry> logs = [];
   List<Drug> drugs = [];
-  Drug? selectedDrug;
 
   TextEditingController notesEditingController = TextEditingController();
   TextEditingController doseEditingController = TextEditingController();
@@ -25,28 +24,10 @@ class _DrugLogPageState extends State<DrugLogPage> {
     setState(() {});
   }
 
-  void setDrug(dynamic drug) {
-    this.selectedDrug = drug;
-    setState(() {});
-  }
-
-  addLog() async {
-    print("sde inserting");
-
-    var selectedDrugId = selectedDrug?.id;
-    if (selectedDrugId != null) {
-      Entry.insertLogWithDrug(notesEditingController.text, selectedDrugId,
-          doseEditingController.text, 1);
-    } else {
-      Entry.insertLog(notesEditingController.text, 1);
-    }
-
-    getLogs();
-    setState(() {});
-    notesEditingController.clear();
-    doseEditingController.clear();
-    selectedDrug = null;
-  }
+  // void setSelectedDrug(dynamic drug) {
+  //   this.selectedDrug = drug;
+  //   setState(() {});
+  // }
 
   getDrugs() async {
     drugs = await Drug.getDrugs();
@@ -61,96 +42,116 @@ class _DrugLogPageState extends State<DrugLogPage> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          print("sde pressed");
           showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              bool includeDrug = false;
-              return AlertDialog(
-                content: StatefulBuilder(
-                  builder: (BuildContext context, StateSetter setState) {
-                    return Container(
-                      width: double.maxFinite,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextField(
-                              controller: notesEditingController,
-                              maxLines: 4, //or null
-                              decoration:
-                                  InputDecoration.collapsed(hintText: "Text"),
-                            ),
+              context: context,
+              builder: (BuildContext context) {
+                bool includeDrug = false;
+                return StatefulBuilder(builder: (context, setState) {
+                  Drug? selectedDrug;
+
+                  addEntry() async {
+                    var selectedDrugId = selectedDrug?.id;
+
+                    if (selectedDrugId != null) {
+                      print("inserting with drug");
+                      Entry.insertLogWithDrug(notesEditingController.text,
+                          selectedDrugId, doseEditingController.text, 1);
+                    } else {
+                      print("inserting without drug");
+                      Entry.insertLog(notesEditingController.text, 1);
+                    }
+
+                    getLogs();
+                    setState(() {});
+                    notesEditingController.clear();
+                    doseEditingController.clear();
+                    Navigator.pop(context);
+                  }
+
+                  setSelectedDrug(Drug drug) {
+                    selectedDrug = drug;
+                    setState(() {});
+                    print("set active drug 2 ${selectedDrug?.name}");
+                  }
+
+                  return AlertDialog(
+                      content: Container(
+                    width: double.maxFinite,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            controller: notesEditingController,
+                            maxLines: 4, //or null
+                            decoration:
+                                InputDecoration.collapsed(hintText: "Text"),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Text("Include drug?"),
-                                Checkbox(
-                                    value: includeDrug,
-                                    checkColor: Colors.blue,
-                                    onChanged: (bool? value) {
-                                      print("sde include drug ${includeDrug}");
-                                      setState(() {
-                                        includeDrug = value!;
-                                      });
-                                    }),
-                              ],
-                            ),
+                        ),
+                        FilledButton(
+                            child: Text("Add entry"),
+                            onPressed: () => addEntry()),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Text("Include drug?"),
+                              Checkbox(
+                                  value: includeDrug,
+                                  checkColor: Colors.blue,
+                                  onChanged: (bool? value) {
+                                    print("sde include drug ${includeDrug}");
+                                    setState(() {
+                                      includeDrug = value!;
+                                    });
+                                  }),
+                            ],
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Row(children: [
-                                  FilledButton(
-                                    onPressed: () {},
-                                    child: Text("Select drug"),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: Text(
-                                        "Selected drug: ${selectedDrug?.name ?? ""}"),
-                                  ),
-                                ]),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(children: [
                                 Padding(
-                                  padding: EdgeInsets.all(2),
-                                  child: TextField(
-                                    controller: doseEditingController,
-                                    decoration: InputDecoration.collapsed(
-                                        hintText: "Dose"),
-                                  ),
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Text(
+                                      "Selected drug: ${selectedDrug?.name ?? "no drug"}"),
                                 ),
-                              ],
-                            ),
+                              ]),
+                              Padding(
+                                padding: EdgeInsets.all(4),
+                                child: TextField(
+                                  controller: doseEditingController,
+                                  decoration: InputDecoration.collapsed(
+                                      hintText: "Dose"),
+                                ),
+                              ),
+                            ],
                           ),
-                          FilledButton(
-                              child: Text("Add a log"),
-                              onPressed: () => addLog()),
-                          Expanded(
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: drugs.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                return OutlinedButton(
-                                  child: Text(drugs[index].name),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                );
-                              },
-                            ),
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: drugs.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return OutlinedButton(
+                                child: Text(drugs[index].id == selectedDrug?.id
+                                    ? "${drugs[index].name} selected"
+                                    : drugs[index].name),
+                                onPressed: () {
+                                  setSelectedDrug(drugs[index]);
+                                },
+                              );
+                            },
                           ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              );
-            },
-          );
+                        ),
+                      ],
+                    ),
+                  ));
+                });
+              });
         },
       ),
       body: Column(
