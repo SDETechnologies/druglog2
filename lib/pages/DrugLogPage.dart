c3865b239b4e45b7e2cb38307886dd14134c5922
DrugLog <androiddruglog@gmail.com>
2023-07-16 15:13:08 -0400

    Moved add entry dialog to its own component

--- lib/pages/DrugLogPage.dart
+++ lib/pages/DrugLogPage.dart
@@ -44,6 +44,63 @@ class _DrugLogPageState extends State<DrugLogPage> {
     getLogs();
     getDrugs();
 
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AddEntryDialog();
+              });
+        },
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                for (var log in logs)
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(.5),
+                              blurRadius: 10,
+                              spreadRadius: 1,
+                              offset: const Offset(5.0, 5.0)),
+                        ],
+                      ),
+                      child: Container(
+                        color: Colors.grey.shade100,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Time: ${log.time}"),
+                              Text("Note: ${log.notes ?? ""}"),
+                              if (log.drugId != null)
+                                Column(
+                                  children: [
+                                    Text("Drug: ${log.drugName}"),
+                                    Text("Dose: ${log.dose}"),
+                                  ],
+                                ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
